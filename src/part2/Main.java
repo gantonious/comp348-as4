26f2a913f286969e9c3ca35f2e4106b4af757798
George Antonious <george@antonious.ca>
2018-03-06 20:45:24 -0700

    Add docs to each main file

--- src/part2/Main.java
+++ src/part2/Main.java
@@ -6,7 +6,23 @@ import email.IEmailSender;
 import email.JavaMailSender;
 
 /**
- * Created by George on 2018-01-03.
+ * title: Main.java
+ * description: The main entry for the SendEmail with attachment program.
+ * date: March 6, 2018
+ * @author George Antonious
+ * @version 1.0
+ * @copyright 2018 George Antonious
+ *
+ * I declare that this assignment is my own work and that all material
+ * previously written or published in any source by any other person
+ * has been duly acknowledged in the assignment. I have not submitted
+ * this work, or a significant part thereof, previously as part of any
+ * academic program. In submitting this assignment I give permission to
+ * copy it for assessment purposes only.
+ *
+ * The usage, design, and test plan for this part can be found in the
+ * README.md file in the root of this project. It is recommended to view
+ * it in a markdown reader.
  */
 public class Main {
 
--- src/part3/Main.java
+++ src/part3/Main.java
@@ -6,7 +6,23 @@ import java.io.PrintStream;
 import java.util.List;
 
 /**
- * Created by George on 2018-01-04.
+ * title: Main.java
+ * description: The main entry for the GetMail program.
+ * date: March 6, 2018
+ * @author George Antonious
+ * @version 1.0
+ * @copyright 2018 George Antonious
+ *
+ * I declare that this assignment is my own work and that all material
+ * previously written or published in any source by any other person
+ * has been duly acknowledged in the assignment. I have not submitted
+ * this work, or a significant part thereof, previously as part of any
+ * academic program. In submitting this assignment I give permission to
+ * copy it for assessment purposes only.
+ *
+ * The usage, design, and test plan for this part can be found in the
+ * README.md file in the root of this project. It is recommended to view
+ * it in a markdown reader.
  */
 public class Main {
 
